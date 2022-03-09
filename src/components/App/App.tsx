813270e0ea1bb1cdaf84f1d8b3eea2a5fa56d28d
Andrii Yukhymenko <andriy111224@gmail.com>
2022-03-09 19:01:02 +0200

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Detail/Detail.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+interface DetailProps {
+  list?: string;
+}
+
+const Detail: FC<DetailProps> = ({ list }) => {
+  return <div></div>;
+};
+
+export default Detail;
